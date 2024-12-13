bc5b72ea3dad343505af4778e0219a9327f24fc7
João Victor <joao.dipaay@gmail.com>
2024-12-12 21:31:46 -0300

    initial config

---
+++ src/test/java/br/com/salus/medical_scheduler/MedicalSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.salus.medical_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
